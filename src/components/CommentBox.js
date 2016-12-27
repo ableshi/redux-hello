0bb19f86d2e4127c2d65e3f4a6cc354833179a6b
Peter Wang <happypeter1983@gmail.com>
2016-12-27 10:40:16 +0800

    show comment list

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 
 
 class CommentBox extends Component {
+  constructor(){
+    super();
+    this.state = {
+      comments: [
+        "hello1",
+        "hello2"
+      ]
+    }
+  }
   render(){
+    let commentList = this.state.comments.map((item, i) => {
+      return (<div key={i} className="comment">
+        { item }
+      </div>)
+    })
     return(
       <div className="comment-box">
-        CommentBox
+        { commentList }
       </div>
     )
   }
