5ace09b5e7ebccc764d689e8fa105f2e589ff99b
Peter Wang <happypeter1983@gmail.com>
2016-12-27 19:55:01 +0800

    dynamic comment.length in PostBody

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import store from '../store';
+import { connect } from 'react-redux';
 
 
 class PostBody extends Component {
-  constructor(){
-    super();
-    this.state = {
-      num: store.getState().length
-    }
-  }
   render(){
     return(
       <div className="post-body">
         <div className="comment-num">
-          { this.state.num }
+          { this.props.comments.length }
         </div>
       </div>
     )
   }
 }
+const mapStateToProps = (state) => ({
+  // 这里的 state 就是 store.getState() 的返回值，也就是整个状态树
+   comments: state
+});
 
-export default PostBody;
+export default connect(mapStateToProps)(PostBody);
+// React 自身特点，props 变化，自动再次呼叫 render()
