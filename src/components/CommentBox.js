89a9ae85908afdb49369471356244d8f8e23c6df
Peter Wang <happypeter1983@gmail.com>
2016-12-27 15:33:40 +0800

    action and reducer

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import store from '../store';
 
 class CommentBox extends Component {
@@ -8,25 +9,30 @@ class CommentBox extends Component {
       comments: store.getState()
     }
   }
-
   handleSubmit(e){
     e.preventDefault();
+    console.log(store.getState());
+    store.dispatch({type: 'ADD_COMMENT', comment: this.refs.comment.value});
+    console.log(store.getState());
     this.refs.commentForm.reset();
+    this.setState({comments: store.getState()});
   }
   render(){
-    let commentList = this.state.comments.map((item, i) => {
-      return (<div key={i} className="comment">
-        { item }
-      </div>)
+    let commentList = this.state.comments.map((comment, i)=>{
+      return (
+        <div className="comment" key={i}>
+          { comment }
+        </div>
+      )
     })
     return(
       <div className="comment-box">
         { commentList }
-        <form ref="commentForm" onSubmit={this.handleSubmit.bind(this)} className="comment-form">
-          <input type="text" className="input" ref="content" />
-          <button type="submit" className="submit-btn" >提交</button>
+        <form ref="commentForm" onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text" ref="comment" className="input"/>
+          <input type="submit" className="submit-btn"/>
+          <div className="underline"></div>
         </form>
-        <div className="underline"></div>
       </div>
     )
   }
