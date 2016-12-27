91cfda13e933765ace3386b46084fc20d9c9c3e5
Billie Zhang <billiecoder@gmail.com>
2016-12-27 20:53:39 +0800

    root reducer

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -6,16 +6,15 @@ class CommentBox extends Component {
   constructor(){
     super();
     this.state = {
-      comments: store.getState()
+      comments: store.getState().comments
     }
   }
   handleSubmit(e){
     e.preventDefault();
     console.log(store.getState());
     store.dispatch({type: 'ADD_COMMENT', comment: this.refs.comment.value});
-    console.log(store.getState());
     this.refs.commentForm.reset();
-    this.setState({comments: store.getState()});
+    this.setState({comments: store.getState().comments});
   }
   render(){
     let commentList = this.state.comments.map((comment, i)=>{
--- src/store.js
+++ src/store.js
@@ -1,22 +1,29 @@
 import { createStore } from 'redux';
-
+import rootReducer from './reducers';
 
 let comments = [
-    "hello1",
-    "hello2"
+  "hello1",
+  "hello2"
 ]
 
-function commentReducer(state = [], action) {
-  // console.log(state, action);
-  switch (action.type) {
-    case 'ADD_COMMENT':
-      // console.log([...state, action.comment])
-      return [...state, action.comment]
-    default:
-      return state;
+const courses = [
+  {
+    id: 1,
+    title: 'redux-hello',
+    likes: 3
+  },
+  {
+    id: 2,
+    title: 'redux-baby',
+    likes: 6
   }
+]
+
+const defaultState = {
+  courses,
+  comments
 }
 
-let store = createStore(commentReducer, comments);
+let store = createStore(rootReducer, defaultState);
 
 export default store;
