b91368526127a997953c1a3dda49bc59aab5c33f
Billie Zhang <billiecoder@gmail.com>
2016-12-28 10:07:01 +0800

    increment likes

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -3,11 +3,18 @@ import store from '../store';
 import { connect } from 'react-redux';
 
 class PostBody extends Component {
+  handleClick(courseId) {
+    store.dispatch({type: 'INCREMENT_LIKES', index: courseId - 1})
+  }
+
   render(){
     return(
       <div className="post-body">
-        <div className="comment-num">
+        <div className="num comments">
           { this.props.comments[this.props.courseId].length }
+        </div>
+        <div className="num likes" onClick={this.handleClick.bind(this, this.props.courseId)}>
+          {this.props.courses[this.props.courseId - 1].likes}
         </div>
       </div>
     )
@@ -15,7 +22,8 @@ class PostBody extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  comments: state.comments
+  comments: state.comments,
+  courses: state.courses
 });
 
 export default connect(mapStateToProps)(PostBody);
